097adc0e672ffb85adc6973b6f39075270c4f620
Oskar Schöldström <m@oxy.fi>
2023-03-16 14:40:57 -0300

    Add node standalone app for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,4 +8,7 @@ export default defineConfig({
 	integrations: [
 		svelte(),
 	],
+	adapter: node({
+		mode: 'standalone'
+	}),
 });
